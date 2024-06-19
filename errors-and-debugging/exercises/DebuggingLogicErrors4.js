4b4012f0463394de824dee5250611b1a1756340c
CodeRod1 <139186202+CodeRod1@users.noreply.github.com>
2024-06-19 17:27:06 -0500

    debugging errors

--- errors-and-debugging/exercises/DebuggingLogicErrors4.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors4.js
@@ -2,6 +2,7 @@
 // Run the code and examine the output. 
 
 // Given the values for fuelLevel, crewStatus and computerStatus, should launchReady be true or false?
+// launchReady should be false.
 // Is the program behaving as expected?
 
 let launchReady = false;
