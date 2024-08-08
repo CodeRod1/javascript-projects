c46386bed09a9e571074a8591021f2f52ea7409f
CodeRod1 <139186202+CodeRod1@users.noreply.github.com>
2024-08-08 16:10:33 -0500

    Add button click functionality to page

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,26 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener("click", function() {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", function () {
+        missionAbort.style.background = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function() {
+        const confirmation = confirm("Are you sure you want to abort the mission?");
+        if (confirmation) {
+            paragraph.innerHTML = '<strong>Mission aborted!</strong> Space shuttle returning home.'
+        }
+    });
+
+
     
 }
 
